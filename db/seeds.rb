b5cf6293a5745391cc41ae8349370ef7912cef42
Duncan Leo <duncanleo97@gmail.com>
2017-08-09 13:47:07 +0800

    move tags.json data to Ruby seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,79 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Player.create!(battle_tag: "Someone-12345", player_icon: "https://example.com/icon.jpg", platform: "pc", region: "us")
+Player.create!(
+  battle_tag: "alphatrl-11833",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
+
+Player.create!(
+  battle_tag: "Autumn-12668",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
+
+Player.create!(
+  battle_tag: "Elchemyyy-1604",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
+
+Player.create!(
+  battle_tag: "ExplosiveCow-1792",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
+
+Player.create!(
+  battle_tag: "FrostyFeet-11226",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
+
+Player.create!(
+  battle_tag: "JxJoker-1331",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
+
+Player.create!(
+  battle_tag: "nkdonut-1804",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
+
+Player.create!(
+  battle_tag: "OopsyDanger-1830",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
+
+Player.create!(
+  battle_tag: "Dad-12262",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
+
+Player.create!(
+  battle_tag: "Wraxu-1747",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
+
+Player.create!(
+  battle_tag: "MemeInhaler-2645",
+  player_icon: "https://example.com/icon.jpg",
+  platform: "pc",
+  region: "us"
+)
