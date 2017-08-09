ad1314dc81f974b47ebbb1750316206e4f9b4eda
Duncan Leo <duncanleo97@gmail.com>
2017-08-09 12:43:44 +0800

    add route to get PlayerData via Player id

--- app/controllers/api/v1/player_datas_controller.rb
+++ app/controllers/api/v1/player_datas_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::PlayerDatasController < Api::V1::BaseController
   def index
     render :json => PlayerData.all
   end
+
+  def show_by_player_id
+    render :json => PlayerData.joins(:player).where(player: { id: params[:id]})
+  end
 end
--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::PlayersController < Api::V1::BaseController
   def index
     render :json => Player.all
   end
+
+  def show
+    render :json => Player.find(params[:id])
+  end
 end
