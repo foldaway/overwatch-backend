6a77ded957e42fcd1cb7e8c7b5c0214597baa8b9
Duncan Leo <duncanleo97@gmail.com>
2017-08-30 12:42:19 +0800

    added basic charts display of SR and Level

--- app/javascript/packs/components/PlayerDetail/index.jsx
+++ app/javascript/packs/components/PlayerDetail/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { LineChart, XAxis, YAxis, CartesianGrid, Line, Tooltip } from 'recharts';
 import Card from '../Card';
+import styles from './styles.scss';
 
 class PlayerDetail extends Component {
   constructor(props) {
@@ -24,9 +26,36 @@ class PlayerDetail extends Component {
   render() {
     if (this.state.player !== null) {
       const { id, battle_tag, player_icon, datas } = this.state.player;
+      const chartData = datas
+        .map(data => Object.assign(data, {
+          created_at: new Date(data.created_at).toLocaleString(),
+        }))
+        .reverse();
       return (
-        <div>
+        <div className={styles.container}>
           <Card playerId={id} battleTag={battle_tag} playerIcon={player_icon} data={Array.isArray(datas) ? datas[0] : null} />
+          <div className={styles.charts}>
+            <div>
+              <h1>SR</h1>
+              <LineChart width={400} height={200} data={chartData} >
+                <XAxis dataKey="created_at" />
+                <YAxis />
+                <Tooltip />
+                <CartesianGrid stroke="#f5f5f5" />
+                <Line type="monotone" dataKey="sr" stroke="#ff7300" yAxisId={0} />
+              </LineChart>
+            </div>
+            <div>
+              <h1>Level</h1>
+              <LineChart width={400} height={200} data={chartData} >
+                <XAxis dataKey="created_at" />
+                <YAxis />
+                <Tooltip />
+                <CartesianGrid stroke="#f5f5f5" />
+                <Line type="monotone" dataKey="level" stroke="#ff7300" yAxisId={0} />
+              </LineChart>
+            </div>
+          </div>
         </div>
       );
     }
