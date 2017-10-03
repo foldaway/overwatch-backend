79096bc7d749e11e3e19589aadbfde1b85c88d2d
Duncan Leo <duncanleo97@gmail.com>
2017-10-03 11:52:15 +0800

    remove all seeds except popular streamers

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,69 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Player.create!(
-  battle_tag: "alphatrl-11833",
-  player_icon: "https://example.com/icon.jpg",
-  platform: "pc",
-  region: "us"
-)
-
-Player.create!(
-  battle_tag: "Autumn-12668",
-  player_icon: "https://example.com/icon.jpg",
-  platform: "pc",
-  region: "us"
-)
-
-Player.create!(
-  battle_tag: "Elchemyyy-1604",
-  player_icon: "https://example.com/icon.jpg",
-  platform: "pc",
-  region: "us"
-)
-
-Player.create!(
-  battle_tag: "ExplosiveCow-1792",
-  player_icon: "https://example.com/icon.jpg",
-  platform: "pc",
-  region: "us"
-)
-
-Player.create!(
-  battle_tag: "FrostyFeet-11226",
-  player_icon: "https://example.com/icon.jpg",
-  platform: "pc",
-  region: "us"
-)
-
-Player.create!(
-  battle_tag: "JxJoker-1331",
-  player_icon: "https://example.com/icon.jpg",
-  platform: "pc",
-  region: "us"
-)
-
-Player.create!(
-  battle_tag: "nkdonut-1804",
-  player_icon: "https://example.com/icon.jpg",
-  platform: "pc",
-  region: "us"
-)
-
-Player.create!(
-  battle_tag: "OopsyDanger-1830",
-  player_icon: "https://example.com/icon.jpg",
-  platform: "pc",
-  region: "us"
-)
-
-Player.create!(
-  battle_tag: "Dad-12262",
-  player_icon: "https://example.com/icon.jpg",
-  platform: "pc",
-  region: "us"
-)
-
-Player.create!(
   battle_tag: "Wraxu-1747",
   player_icon: "https://example.com/icon.jpg",
   platform: "pc",
@@ -77,7 +14,7 @@ Player.create!(
 )
 
 Player.create!(
-  battle_tag: "MemeInhaler-2645",
+  battle_tag: "Seagull-1894",
   player_icon: "https://example.com/icon.jpg",
   platform: "pc",
   region: "us"
