bc1596f964e7f24c99bce98061d5bda47bd5fe1b
Duncan Leo <hello@duncanleo.me>
2020-08-15 00:44:31 +0800

    fix: CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'owdash.netlify.com', 'localhost:1234'
+    origins 'owdash.netlify.app', 'localhost:1234'
 
     resource '*',
       headers: :any,
